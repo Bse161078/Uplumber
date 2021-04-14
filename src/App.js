4740db50a003a4e6382bd2498750bf139f34dd4d
UmairMukhtar <umairmukhtar27@gmail.com>
2021-04-14 13:17:33 +0500

    moroe'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
 import LoginPage from "./Pages/LoginPage";
 import CreateAccount from "./Pages/CreateAccount";
 import CompleteProfile from "./Pages/CompleteProfile";
@@ -12,13 +13,15 @@ export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" render={() => <LoginPage />} />
+        <Route exact path="/login" render={() => <LoginPage />} />
+
         <Route exact path="/create-account" render={() => <CreateAccount />} />
         <Route
           exact
           path="/complete-profile"
           render={() => <CompleteProfile />}
         />
+        <Route exact path="/" render={() => <LandingPage />} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import UPlumberLogo from "../assets/uplumberlogo.png";
+import Plumbers from "../assets/Plumbers.png";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: "white",
+    border: "none",
+    borderRadius: 15,
+    width: "80%",
+    background: "#1075c2",
+    height: 45,
+    marginTop: 20,
+  },
+}));
+
+export default function LandingPage() {
+  const classes = useStyles();
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        background: "linear-gradient(to right, #F6F6F6 50%, #F2F2F2 50%)",
+      }}
+    >
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        style={{ height: "50vh" }}
+      >
+        <img src={UPlumberLogo} style={{ width: "90%" }}></img>
+      </Grid>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        style={{
+          height: "inherit",
+          background: `url(${Plumbers})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          //   height: "50vh",
+        }}
+      >
+        <button className={classes.button} style={{ marginTop: 200 }}>
+          <Grid container direction="row" justify="center" alignItems="center">
+            Start Exploring <ArrowRightAltIcon></ArrowRightAltIcon>
+          </Grid>
+        </button>
+        <p style={{ width: "80%", textAlign: "center" }}>
+          By continuing you agreeto our{" "}
+          <span style={{ fontWeight: "bold" }}>Terms and conditions</span> and{" "}
+          <span style={{ fontWeight: "bold" }}>Privay Policy</span>
+        </p>
+      </Grid>
+    </div>
+  );
+}
